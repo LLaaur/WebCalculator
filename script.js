26a38b0d2f93f7533cfd00d7160d2fe383829fcd
Laur <laur_laurentiu2013@yahoo.com>
2022-07-05 18:59:46 +0300

    add functionality to clear buttons

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const screen = document.querySelector('.calculator-display');
 const showUp = document.querySelectorAll('.show');
+const remove = document.querySelector('.del');
+const allClear = document.querySelector('.ac');
+
+
+const clearingBtns = function () {
+    if (allClear) {
+        allClear.addEventListener('click', () =>  screen.textContent = '' )
+    };
+    if (remove){ 
+        remove.addEventListener('click', () => screen.textContent = screen.textContent.toString().slice(0, -1))
+    };
+};
+
+clearingBtns();
 
 const displayContent = function(){
     showUp.forEach(button => {
         button.addEventListener('click', () => {
-            screen.textContent = button.textContent;
+            screen.textContent += button.textContent;
         });
     });
 };
