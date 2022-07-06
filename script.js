31a47bfd5c3e8f08f6079e96711c84ba21f8250c
Laur <laur_laurentiu2013@yahoo.com>
2022-07-06 20:16:47 +0300

    small refactoring and bug fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const screen = document.querySelector('.calculator-display');
-const numbers = document.querySelectorAll('.show');
+const numbers = document.querySelectorAll('.show.number');
 const operators = document.querySelectorAll('.show.operator');
 const remove = document.querySelector('.del');
 const allClear = document.querySelector('.ac');
@@ -13,18 +13,21 @@ let cleanCalculatorDisplay = false;
 const currentOperationDisplay = document.querySelector('.current-display');
 const lastOperationDisplay = document.querySelector('.last-display');
 
-const clearAll = function(){
+const clearAll = function () {
     currentOperationDisplay.textContent = '0';
     lastOperationDisplay.textContent = '';
-}
+    operandX = '';
+    operandY = '';
+    currentOperation = null;
+};
 
 allClear.addEventListener('click', clearAll);
 
-const removeNumber = function(){
+const removeElement = function () {
     currentOperationDisplay.textContent = currentOperationDisplay.textContent.toString().slice(0, -1);
 }
 
-remove.addEventListener('click', removeNumber);
+remove.addEventListener('click', removeElement);
 
 const displayNumbers = function (number) {
     if (currentOperationDisplay.textContent === '0' || cleanCalculatorDisplay) {
@@ -38,28 +41,28 @@ numbers.forEach(button => {
     button.addEventListener('click', () => displayNumbers(button.textContent))
 });
 
-operators.forEach(button =>{
+operators.forEach(button => {
     button.addEventListener('click', () => declareOperation(button.textContent))
 })
 
 
-const cleanDisplay = function(){
+const cleanDisplay = function () {
     currentOperationDisplay.textContent = '';
     cleanCalculatorDisplay = false;
 }
 
 
-const declareOperation = function(operator){
+const declareOperation = function (operator) {
     if (currentOperation !== null) calculate();
     operandX = currentOperationDisplay.textContent;
     currentOperation = operator;
-    lastOperationDisplay.textContent = `${operandX}`;
+    lastOperationDisplay.textContent = `${operandX} ${currentOperation}`;
     cleanCalculatorDisplay = true;
 }
 
-const calculate = function(){
-    if(currentOperation === null || cleanCalculatorDisplay) return;
-    if (currentOperation === '÷' && currentOperationDisplay.textContent === '0'){
+const calculate = function () {
+    if (currentOperation === null || cleanCalculatorDisplay) return;
+    if (currentOperation === '÷' && currentOperationDisplay.textContent === '0') {
         alert('Division by 0 not possible')
         return
     };
@@ -67,44 +70,44 @@ const calculate = function(){
     currentOperationDisplay.textContent = roundNumber(
         operate(currentOperation, operandX, operandY)
     );
-    lastOperationDisplay.textContent = `${operandX}${operandY} ${'='}`
+    lastOperationDisplay.textContent = `${operandX} ${currentOperation} ${operandY} ${'='}`
     currentOperation = null
 };
 
-const roundNumber = function(num){
+const roundNumber = function (num) {
     return Math.round(num * 1000) / 1000;
 };
 
-const add = function(x, y){
+const add = function (x, y) {
     return x + y
 };
 
-const subtract = function(x, y){
+const subtract = function (x, y) {
     return x - y
 };
 
-const multiply = function(x, y){
+const multiply = function (x, y) {
     return x * y
 };
 
-const divide = function(x, y){
+const divide = function (x, y) {
     return x / y
 }
 
-const operate = function(operator, x, y){
+const operate = function (operator, x, y) {
     x = parseInt(x);
     y = parseInt(y);
     // CHECKING FOR ALL OPERATORS
-    if (operator === '+'){
-        return add (x, y)
+    if (operator === '+') {
+        return add(x, y)
     }
-    else if (operator === '-'){
+    else if (operator === '-') {
         return subtract(x, y)
     }
-    else if (operator === '*'){
+    else if (operator === '*') {
         return multiply(x, y)
     }
-    else if (operator === '÷'){
+    else if (operator === '÷') {
         return divide(x, y)
     }
     else {
@@ -113,4 +116,4 @@ const operate = function(operator, x, y){
 };
 
 const equal = document.querySelector('.equals');
-equal.addEventListener('click', calculate)
+equal.addEventListener('click', calculate);
